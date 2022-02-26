f353a9cb86fd8913402bb3ddbd4df98508eb8961
Calil-Silva <ka.renner@hotmail.com>
2022-02-26 02:20:29 -0300

    feat: add ormconfig.ts

---
+++ ormconfig.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    migrationsTableName: 'migrations',
+    entities: ['dist/entities/*.js'],
+    migrations: ['dist/migrations/*.js'],
+    // extra: {
+    //     ssl: {
+    //         rejectUnauthorized: false,
+    //     },
+    // },
+    cli: {
+        migrationsDir: 'src/migrations',
+        entitiesDir: 'dist/entities/*.js',
+    },
+};
