c4b76c5001a4524870ae93d5c7f9481f6819dc55
Calil-Silva <ka.renner@hotmail.com>
2022-03-02 14:28:01 -0300

    fix: add contact, number and email to customers entity

--- src/migrations/1646238248500-customerData.ts
+++ src/migrations/1646238248500-customerData.ts
@@ -5,21 +5,21 @@ export class customerData1646238248500 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE "customers" ADD "email" character varying NOT NULL`,
+            'ALTER TABLE "customers" ADD "email" character varying NOT NULL',
         );
         await queryRunner.query(
-            `ALTER TABLE "customers" ADD "contact" character varying NOT NULL`,
+            'ALTER TABLE "customers" ADD "contact" character varying NOT NULL',
         );
         await queryRunner.query(
-            `ALTER TABLE "customers" ADD "number" character varying NOT NULL`,
+            'ALTER TABLE "customers" ADD "number" character varying NOT NULL',
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "number"`);
+        await queryRunner.query('ALTER TABLE "customers" DROP COLUMN "number"');
         await queryRunner.query(
-            `ALTER TABLE "customers" DROP COLUMN "contact"`,
+            'ALTER TABLE "customers" DROP COLUMN "contact"',
         );
-        await queryRunner.query(`ALTER TABLE "customers" DROP COLUMN "email"`);
+        await queryRunner.query('ALTER TABLE "customers" DROP COLUMN "email"');
     }
 }
