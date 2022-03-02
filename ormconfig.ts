4c28ab5a8eeec77333f5c72a4525f35460baef06
Calil-Silva <ka.renner@hotmail.com>
2022-03-02 16:47:49 -0300

    fix: change orm config from import to require

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,8 +1,7 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 
 dotenv.config();
-
-export default {
+module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     migrationsTableName: 'migrations',
